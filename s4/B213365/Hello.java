fe956ad942a06a37841ec507fd7f3212fa0cc900
Matsumoto <matsumoto.katsuaki.me@tut.jp>
2021-12-07 17:41:34 +0900

    Hello.java local

--- s4/B213365/Hello.java
+++ s4/B213365/Hello.java
@@ -3,6 +3,6 @@ import java.lang.*;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("merge(B213365)");
+        System.out.println("local(B213365)");
     }
 }
