dad812f7534f735c899bdeda8a2fd27573fe6c6f
tut213365 <95677091+tut213365@users.noreply.github.com>
2021-12-07 17:28:48 +0900

    Update Hello.java

--- s4/B213365/Hello.java
+++ s4/B213365/Hello.java
@@ -3,6 +3,6 @@ import java.lang.*;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("github(B213365)");
+        System.out.println("merge(B213365)");
     }
 }
